cf3f370e543755a8663cb953421175385bb206df
hillpig <hillpig@gmail.com>
2018-01-22 18:01:07 -0800

    add Stacktester.cpp and modify the previous code

--- Stack/Stack/Stack.hpp
+++ Stack/Stack/Stack.hpp
@@ -1,3 +1,20 @@
-//Stack.hpp
+//Stack.hpp headerfile of the project
 #include <iostream>
+#include <iomanip>
 using namespace std;
+class Stack
+{
+	static constexpr int size{10};
+	int container[size];
+	int topElement;
+public:
+	Stack();
+	void push(int input);
+	void pop();
+	const int top();
+	const bool empty();
+	const bool full();
+	void print();
+private:
+
+};
--- Stack/Stack/Stacktester.cpp
+++ Stack/Stack/Stacktester.cpp
@@ -1,6 +1,31 @@
 //Stacktester.cpp: Test the class of Stack
+#include "Stack.hpp"
 
 int main()
 {
+	Stack test;
+	static int i{ 0 };
+	if (test.empty()) {
+		while (!test.full()) {
+			test.push(i++);
+		}
+	}                                          // push integers into the stack until full
+	test.print();                              // print each element in the stack
+	test.push(i);                              // try to push a new integer into the stack
+	cout << "Push a new integer into a full stack, the index of the top element is still: " << test.top() << endl; // find the top index
+	if (test.full()) {
+		while (!test.empty()) {
+			test.pop();
+		}
+	}                                           // delete all elements in the stack
+	cout <<"For a empty stack, the index of the top element is: "<< test.top() << endl;        // find the top index
+	test.pop();                                  // try to delete the top from the stack
+	if (test.empty()) {
+		while (!test.full()) {
+			test.push(i++);
+		}
+	}                                             // push other integers into the stack until full
+	test.print();                                 // print all elements in the stack
+	cout <<"Now the index of the top element is: " << test.top() << endl;                       //find the top index
 	return 0;
 }
